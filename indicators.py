090893ef48023dbee8076e7e15a759739e1b237b
mahendra5445 <mahendrachaudhary56@gmail.com>
2026-07-18 04:11:52 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,11 +1,32 @@
+
+import pandas as pd
+
 def ema(values, period):
-    if len(values) < period:
-        return None
+    return round(pd.Series(values).ewm(span=period, adjust=False).mean().iloc[-1], 2)
 
-    multiplier = 2 / (period + 1)
-    ema_value = sum(values[:period]) / period
+def rsi(values, period=14):
+    close = pd.Series(values)
+    delta = close.diff()
 
-    for price in values[period:]:
-        ema_value = (price - ema_value) * multiplier + ema_value
+    gain = delta.where(delta > 0, 0).rolling(period).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(period).mean()
 
-    return round(ema_value, 2)
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+
+    return round(rsi.iloc[-1], 2)
+
+def macd(values):
+    close = pd.Series(values)
+
+    ema12 = close.ewm(span=12, adjust=False).mean()
+    ema26 = close.ewm(span=26, adjust=False).mean()
+
+    macd_line = ema12 - ema26
+    signal_line = macd_line.ewm(span=9, adjust=False).mean()
+
+    return {
+        "macd": round(macd_line.iloc[-1], 2),
+        "signal": round(signal_line.iloc[-1], 2),
+        "trend": "Bullish" if macd_line.iloc[-1] > signal_line.iloc[-1] else "Bearish"
+    }
