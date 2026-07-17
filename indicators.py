e3895f7261edf6bc242fcf88286b7d1cfa3fd5b0
mahendra5445 <mahendrachaudhary56@gmail.com>
2026-07-18 03:54:48 +0530

    Create indicators.py

---
+++ indicators.py
@@ -0,0 +1,11 @@
+def ema(values, period):
+    if len(values) < period:
+        return None
+
+    multiplier = 2 / (period + 1)
+    ema_value = sum(values[:period]) / period
+
+    for price in values[period:]:
+        ema_value = (price - ema_value) * multiplier + ema_value
+
+    return round(ema_value, 2)
