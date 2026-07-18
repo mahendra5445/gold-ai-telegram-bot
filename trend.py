b6530aa318af08dd3bbcb95351f0dfcddce25134
mahendra5445 <mahendrachaudhary56@gmail.com>
2026-07-18 22:58:40 +0530

    Update trend.py

--- trend.py
+++ trend.py
@@ -1,31 +1,15 @@
 from indicators import ema
 
 
-def analyze_trend(close_prices):
+def get_trend(close_prices):
     ema20 = ema(close_prices, 20)
     ema50 = ema(close_prices, 50)
     ema200 = ema(close_prices, 200)
 
-    score = 50
-    trend = "Sideways"
-
     if ema20 > ema50 > ema200:
-        trend = "Strong Bullish"
-        score = 90
+        return "Bullish"
 
-    elif ema20 > ema50:
-        trend = "Bullish"
-        score = 75
+    if ema20 < ema50 < ema200:
+        return "Bearish"
 
-    elif ema20 < ema50 < ema200:
-        trend = "Strong Bearish"
-        score = 90
-
-    elif ema20 < ema50:
-        trend = "Bearish"
-        score = 75
-
-    return {
-        "trend": trend,
-        "score": score
-    }
+    return "Sideways"
