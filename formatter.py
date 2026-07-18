31820f5891ec2cd7cc86a720671490513e44b88a
mahendra5445 <mahendrachaudhary56@gmail.com>
2026-07-18 13:05:50 +0530

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,31 +1,108 @@
 def format_signal(candles, result):
-    message = (
-        "📊 GOLD AI SCALPER PRO\n\n"
-        f"💰 Price : {candles['price']}\n\n"
-        f"📈 EMA20 : {result['ema20']}\n"
-        f"📈 EMA50 : {result['ema50']}\n"
-        f"📈 EMA200 : {result['ema200']}\n\n"
-        f"📉 RSI : {result['rsi']}\n"
-        f"📊 MACD : {result['macd']['trend']}\n\n"
-        f"🎯 Signal : {result['signal']}\n"
-    )
+    """
+    Format Telegram Message
+    """
 
-    if result["entry"] is not None:
-        message += (
-            f"\n💰 Entry : {result['entry']}\n"
-            f"🛑 Stop Loss : {result['sl']}\n"
-            f"🎯 TP1 : {result['tp1']}\n"
-            f"🎯 TP2 : {result['tp2']}\n"
-            f"\n📈 Confidence : {result['confidence']}%"
-        )
+    trend = result["trend"]["trend"]
+    trend_score = result["trend"]["score"]
 
-        if "risk_reward" in result:
-            message += f"\n⚖️ Risk : Reward : {result['risk_reward']}"
+    reasons = result["reason"]
 
-        if "atr" in result:
-            message += f"\n📉 ATR : {result['atr']}"
+    if len(reasons) == 0:
+        reason_text = "No confirmation"
 
-        if "trend_strength" in result:
-            message += f"\n📊 Trend : {result['trend_strength']}"
+    else:
+        reason_text = "\n".join([f"• {x}" for x in reasons])
+
+    smc = result["smart_money"]
+
+    mtf_text = "Not Available"
+
+    if result["mtf"]:
+
+        mtf = result["mtf"]
+
+        mtf_text = ""
+
+        for tf, signal in mtf["timeframes"].items():
+            mtf_text += f"{tf} : {signal}\n"
+
+        mtf_text += f"\nOverall : {mtf['overall']}"
+
+    message = f"""
+🤖 GOLD AI SCALPER PRO V3
+
+━━━━━━━━━━━━━━━━━━
+
+📈 Signal : {result['signal']}
+
+💰 Price : {result['price']}
+
+🎯 Entry : {result['entry']}
+
+🛑 Stop Loss : {result['sl']}
+
+✅ TP1 : {result['tp1']}
+
+✅ TP2 : {result['tp2']}
+
+━━━━━━━━━━━━━━━━━━
+
+🤖 AI Score : {result['score']}
+
+📊 Confidence : {result['confidence']}%
+
+📈 Trend : {trend}
+
+🔥 Trend Score : {trend_score}
+
+━━━━━━━━━━━━━━━━━━
+
+📊 EMA20 : {round(result['ema20'],2)}
+
+📊 EMA50 : {round(result['ema50'],2)}
+
+📊 EMA200 : {round(result['ema200'],2)}
+
+RSI : {round(result['rsi'],2)}
+
+ATR : {round(result['atr'],2)}
+
+━━━━━━━━━━━━━━━━━━
+
+🧠 SMART MONEY
+
+BOS : {"✅" if smc["bos"] else "❌"}
+
+CHOCH : {"✅" if smc["choch"] else "❌"}
+
+ORDER BLOCK : {smc["order_block"]}
+
+FVG : {"✅" if smc["fvg"] else "❌"}
+
+LIQUIDITY : {"✅" if smc["liquidity"] else "❌"}
+
+━━━━━━━━━━━━━━━━━━
+
+⏰ MULTI TIMEFRAME
+
+{mtf_text}
+
+━━━━━━━━━━━━━━━━━━
+
+⚖ Risk Reward
+
+{result['risk_reward']}
+
+━━━━━━━━━━━━━━━━━━
+
+💡 WHY THIS TRADE?
+
+{reason_text}
+
+━━━━━━━━━━━━━━━━━━
+
+⚠ Trade Responsibly
+"""
 
     return message
