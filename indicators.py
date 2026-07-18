c1267d26f54ed23393acfc370e119c17e1f30a66
mahendra5445 <mahendrachaudhary56@gmail.com>
2026-07-18 11:42:23 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,20 +1,26 @@
-
 import pandas as pd
 
+
 def ema(values, period):
-    return round(pd.Series(values).ewm(span=period, adjust=False).mean().iloc[-1], 2)
+    return round(
+        pd.Series(values).ewm(span=period, adjust=False).mean().iloc[-1], 2
+    )
+
 
 def rsi(values, period=14):
     close = pd.Series(values)
+
     delta = close.diff()
 
     gain = delta.where(delta > 0, 0).rolling(period).mean()
     loss = (-delta.where(delta < 0, 0)).rolling(period).mean()
 
     rs = gain / loss
+
     rsi = 100 - (100 / (1 + rs))
 
     return round(rsi.iloc[-1], 2)
+
 
 def macd(values):
     close = pd.Series(values)
@@ -25,8 +31,34 @@ def macd(values):
     macd_line = ema12 - ema26
     signal_line = macd_line.ewm(span=9, adjust=False).mean()
 
+    trend = "Bullish"
+
+    if macd_line.iloc[-1] < signal_line.iloc[-1]:
+        trend = "Bearish"
+
     return {
         "macd": round(macd_line.iloc[-1], 2),
         "signal": round(signal_line.iloc[-1], 2),
-        "trend": "Bullish" if macd_line.iloc[-1] > signal_line.iloc[-1] else "Bearish"
+        "trend": trend,
     }
+
+
+def atr(high, low, close, period=14):
+    high = pd.Series(high)
+    low = pd.Series(low)
+    close = pd.Series(close)
+
+    previous_close = close.shift(1)
+
+    tr1 = high - low
+    tr2 = (high - previous_close).abs()
+    tr3 = (low - previous_close).abs()
+
+    true_range = pd.concat(
+        [tr1, tr2, tr3],
+        axis=1
+    ).max(axis=1)
+
+    atr = true_range.rolling(period).mean()
+
+    return round(atr.iloc[-1], 2)
