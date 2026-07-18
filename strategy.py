a5bfe7166b65178319f6490594b32bffaff10deb
mahendra5445 <mahendrachaudhary56@gmail.com>
2026-07-18 11:50:44 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,15 +1,19 @@
-from indicators import ema, rsi, macd
+from indicators import ema, rsi, macd, atr
+from risk import calculate_trade
 
-def get_signal(close_prices):
+
+def get_signal(close_prices, high_prices, low_prices):
     ema20 = ema(close_prices, 20)
     ema50 = ema(close_prices, 50)
     ema200 = ema(close_prices, 200)
 
     rsi_value = rsi(close_prices)
     macd_data = macd(close_prices)
+    atr_value = atr(high_prices, low_prices, close_prices)
 
     signal = "NO TRADE"
     confidence = 50
+    trend_strength = "Sideways"
 
     if (
         ema20 > ema50 > ema200
@@ -17,7 +21,8 @@ def get_signal(close_prices):
         and macd_data["trend"] == "Bullish"
     ):
         signal = "BUY"
-        confidence = 90
+        confidence = 90 if rsi_value > 60 else 80
+        trend_strength = "Strong Bullish"
 
     elif (
         ema20 < ema50 < ema200
@@ -25,24 +30,16 @@ def get_signal(close_prices):
         and macd_data["trend"] == "Bearish"
     ):
         signal = "SELL"
-        confidence = 90
+        confidence = 90 if rsi_value < 40 else 80
+        trend_strength = "Strong Bearish"
 
-    price = close_prices[-1]
+    price = round(close_prices[-1], 2)
 
-    if signal == "BUY":
-        entry = round(price, 2)
-        sl = round(price - 5, 2)
-        tp1 = round(price + 10, 2)
-        tp2 = round(price + 20, 2)
-
-    elif signal == "SELL":
-        entry = round(price, 2)
-        sl = round(price + 5, 2)
-        tp1 = round(price - 10, 2)
-        tp2 = round(price - 20, 2)
-
-    else:
-        entry = sl = tp1 = tp2 = None
+    trade = calculate_trade(
+        signal=signal,
+        price=price,
+        atr=atr_value
+    )
 
     return {
         "ema20": ema20,
@@ -50,10 +47,13 @@ def get_signal(close_prices):
         "ema200": ema200,
         "rsi": rsi_value,
         "macd": macd_data,
+        "atr": atr_value,
         "signal": signal,
-        "entry": entry,
-        "sl": sl,
-        "tp1": tp1,
-        "tp2": tp2,
+        "entry": trade["entry"],
+        "sl": trade["sl"],
+        "tp1": trade["tp1"],
+        "tp2": trade["tp2"],
+        "risk_reward": trade["risk_reward"],
         "confidence": confidence,
+        "trend_strength": trend_strength,
     }
