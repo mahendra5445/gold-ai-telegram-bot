ab675b23176cf56cf19c0a83b4ae322c55c39b08
mahendra5445 <mahendrachaudhary56@gmail.com>
2026-07-18 22:18:09 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -3,37 +3,64 @@ from risk import calculate_trade
 
 
 def get_signal(close_prices, high_prices, low_prices):
-    ema20 = ema(close_prices, 20)
-    ema50 = ema(close_prices, 50)
-    ema200 = ema(close_prices, 200)
+    ema20 = round(ema(close_prices, 20), 2)
+    ema50 = round(ema(close_prices, 50), 2)
+    ema200 = round(ema(close_prices, 200), 2)
 
-    rsi_value = rsi(close_prices)
+    rsi_value = round(rsi(close_prices), 2)
     macd_data = macd(close_prices)
-    atr_value = atr(high_prices, low_prices, close_prices)
+    atr_value = round(atr(high_prices, low_prices, close_prices), 2)
+
+    price = round(close_prices[-1], 2)
 
     signal = "NO TRADE"
     confidence = 50
     trend_strength = "Sideways"
 
+    # Trend distance
+    ema_gap = abs(ema20 - ema50)
+
+    # Strong Bullish
     if (
         ema20 > ema50 > ema200
-        and rsi_value > 55
+        and ema_gap > atr_value * 0.10
+        and rsi_value >= 60
         and macd_data["trend"] == "Bullish"
     ):
         signal = "BUY"
-        confidence = 90 if rsi_value > 60 else 80
+        confidence = 95
         trend_strength = "Strong Bullish"
 
+    # Medium Bullish
+    elif (
+        ema20 > ema50 > ema200
+        and rsi_value >= 55
+        and macd_data["trend"] == "Bullish"
+    ):
+        signal = "BUY"
+        confidence = 85
+        trend_strength = "Bullish"
+
+    # Strong Bearish
     elif (
         ema20 < ema50 < ema200
-        and rsi_value < 45
+        and ema_gap > atr_value * 0.10
+        and rsi_value <= 40
         and macd_data["trend"] == "Bearish"
     ):
         signal = "SELL"
-        confidence = 90 if rsi_value < 40 else 80
+        confidence = 95
         trend_strength = "Strong Bearish"
 
-    price = round(close_prices[-1], 2)
+    # Medium Bearish
+    elif (
+        ema20 < ema50 < ema200
+        and rsi_value <= 45
+        and macd_data["trend"] == "Bearish"
+    ):
+        signal = "SELL"
+        confidence = 85
+        trend_strength = "Bearish"
 
     trade = calculate_trade(
         signal=signal,
